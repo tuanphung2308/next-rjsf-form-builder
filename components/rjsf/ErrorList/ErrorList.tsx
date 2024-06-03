73120a27cb97ded1d719521e093c773ade9beaa8
tuanphung <tuan.phung@ai4biz.tech>
2024-06-03 17:56:18 +0700

    feat: format error list container

--- components/rjsf/ErrorList/ErrorList.tsx
+++ components/rjsf/ErrorList/ErrorList.tsx
@@ -1,34 +1,36 @@
 import {
-  ErrorListProps,
-  FormContextType,
-  RJSFSchema,
-  StrictRJSFSchema,
-  TranslatableString,
+    ErrorListProps,
+    FormContextType,
+    RJSFSchema,
+    StrictRJSFSchema,
+    TranslatableString,
 } from "@rjsf/utils"
+import React from "react";
+import {OctagonAlert, Terminal} from "lucide-react";
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
 
 export default function ErrorList<
-  T = any,
-  S extends StrictRJSFSchema = RJSFSchema,
-  F extends FormContextType = any,
->({ errors, registry }: ErrorListProps<T, S, F>) {
-  const { translateString } = registry
+    T = any,
+    S extends StrictRJSFSchema = RJSFSchema,
+    F extends FormContextType = any,
+>({errors, registry}: ErrorListProps<T, S, F>) {
+    const {translateString} = registry
 
-  return (
-    <div className="mb-4 rounded border border-red-700">
-      <div className="rounded-t bg-red-100 p-3 text-red-950">
-        {translateString(TranslatableString.ErrorsLabel)}
-      </div>
-      <div className="p-0">
-        <ul>
-          {errors.map((error, i: number) => {
-            return (
-              <li key={i} className="border-0 p-3">
-                <span>{error.stack}</span>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    </div>
-  )
+    return (
+        <Alert className="mb-4">
+            <OctagonAlert size={16}/>
+            <AlertTitle>
+                {translateString(TranslatableString.ErrorsLabel)}
+            </AlertTitle>
+            <AlertDescription>
+                {errors.map((error, i: number) => {
+                    return (
+                        <li key={i} className="border-0 p-1">
+                            <span>{error.stack}</span>
+                        </li>
+                    )
+                })}
+            </AlertDescription>
+        </Alert>
+    )
 }
--- samples/customFieldAnyOf.tsx
+++ samples/customFieldAnyOf.tsx
@@ -1,108 +1,88 @@
+import React from "react";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+
 const UiField: React.FC<{
-  schema?: any;
-  idSchema: { $id: string };
-  formData?: any;
-  onChange: (...args: any[]) => void;
-  [key: string]: any;
-}> = ({ idSchema: { $id }, formData, onChange }) => {
-  const changeHandlerFactory = (fieldName: string) => (event: any) => {
-    onChange(formData ? { ...formData, [fieldName]: event.target.value } : { [fieldName]: event.target.value });
-  };
-  return (
-    <>
-      <h4>Location</h4>
-      <div style={{ display: 'flex' }}>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '50%',
-            margin: '1rem',
-          }}
-        >
-          <div className='form-group field field-string'>
-            <label className='control-label' htmlFor={`${$id}-city`}>
-              City
-            </label>
-            <input
-              className='form-control'
-              id={`${$id}-city`}
-              required={false}
-              placeholder=''
-              type='text'
-              value={formData?.city || ''}
-              onChange={changeHandlerFactory('city')}
-            />
-          </div>
+    schema?: any;
+    idSchema: { $id: string };
+    formData?: any;
+    onChange: (...args: any[]) => void;
+    [key: string]: any;
+}> = ({idSchema: {$id}, formData, onChange}) => {
+    const changeHandlerFactory = (fieldName: string) => (event: any) => {
+        onChange(formData ? {...formData, [fieldName]: event.target.value} : {[fieldName]: event.target.value});
+    };
+    return (
+        <div className="flex flex-col gap-2">
+            <h4>Location</h4>
+            <div className="flex gap-2">
+                <div className="flex flex-col flex-1">
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                        <Label htmlFor={`${$id}-city`}>City</Label>
+                        <Input
+                            className='form-control'
+                            id={`${$id}-city`}
+                            required={false}
+                            placeholder=''
+                            type='text'
+                            value={formData?.city || ''}
+                            onChange={changeHandlerFactory('city')}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                        <Label htmlFor={`${$id}-lat`}>Latitude</Label>
+                        <Input className='form-control'
+                               id={`${$id}-lat`}
+                               type='number'
+                               value={formData?.lat || 0}
+                               onChange={changeHandlerFactory('lat')}/>
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                        <Label htmlFor={`${$id}-lon`}>Longitude</Label>
+                        <Input className='form-control'
+                               id={`${$id}-lon`}
+                               type='number'
+                               value={formData?.lon || 0}
+                               onChange={changeHandlerFactory('lon')}/>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '50%',
-            margin: '1rem',
-          }}
-        >
-          <div className='form-group field field-string'>
-            <label className='control-label' htmlFor={`${$id}-lat`}>
-              Latitude
-            </label>
-            <input
-              className='form-control'
-              id={`${$id}-lat`}
-              type='number'
-              value={formData?.lat || 0}
-              onChange={changeHandlerFactory('lat')}
-            />
-          </div>
-          <div className='form-group field field-string'>
-            <label className='control-label' htmlFor={`${$id}-lon`}>
-              Longitude
-            </label>
-            <input
-              className='form-control'
-              id={`${$id}-lon`}
-              type='number'
-              value={formData?.lon || 0}
-              onChange={changeHandlerFactory('lon')}
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    );
 };
 
 export default {
-  schema: {
-    title: 'Location',
-    type: 'object',
-    anyOf: [
-      {
-        title: 'City',
-        properties: {
-          city: {
-            type: 'string',
-          },
-        },
-        required: ['city'],
-      },
-      {
-        title: 'Coordinates',
-        properties: {
-          lat: {
-            type: 'number',
-          },
-          lon: {
-            type: 'number',
-          },
-        },
-        required: ['lat', 'lon'],
-      },
-    ],
-  },
-  uiSchema: {
-    'ui:field': UiField,
-  },
-  formData: {},
+    schema: {
+        title: 'Location',
+        type: 'object',
+        anyOf: [
+            {
+                title: 'City',
+                properties: {
+                    city: {
+                        type: 'string',
+                    },
+                },
+                required: ['city'],
+            },
+            {
+                title: 'Coordinates',
+                properties: {
+                    lat: {
+                        type: 'number',
+                    },
+                    lon: {
+                        type: 'number',
+                    },
+                },
+                required: ['lat', 'lon'],
+            },
+        ],
+    },
+    uiSchema: {
+        'ui:field': UiField,
+    },
+    formData: {},
 };
